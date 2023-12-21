9303c3d6bb5b14b425f67924f243db09aa752575
swapnil chavan <chavanswapnil822@gmail.com>
2023-12-21 14:44:31 +0530

    updated game

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,10 @@
-import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import { Login } from './Pages/Login';
-import { Navbar } from './Pages/Navbar';
-import { Products } from './Pages/Products';
-import { Cart } from './Pages/Cart';
-import PaymentPage from './Pages/PaymentPage';
 import Game from './Components/Game';
 
 function App() {
   return (
     <div className="App">
-      
-      {/* <Navbar /> */}
       <Game />
-      {/* <Routes>
-          <Route index element={<Login />} />
-          <Route path="products" element={<Products />} />
-          <Route path="cart" element={<Cart />} />
-          <Route path="cart/payment" element={<PaymentPage />} />
-      </Routes> */}
-
     </div>
   );
 }
