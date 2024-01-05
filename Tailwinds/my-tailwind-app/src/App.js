c88cef1adb8d95e7bf031eef92d7058f2af55043
swapnil chavan <chavanswapnil822@gmail.com>
2024-01-05 16:54:45 +0530

    updated code with tailwins css

---
+++ Tailwinds/my-tailwind-app/src/App.js
@@ -0,0 +1,25 @@
+// src/App.js
+import React from 'react';
+
+function App() {
+  return (
+    <>
+    <div className="bg-blue-500 text-white p-4">
+      <div className='flex gp 2'>
+        <h1 className='text-3xl'>Hello</h1>
+        <h2>Bye</h2>
+        <h1>Namaste</h1>
+      </div>
+      <h1 className="text-3xl font-bold">Hello, Tailwind CSS!</h1>
+      <p className="mt-2">This is a React app with Tailwind CSS styling.</p>
+      
+    </div>
+
+    
+
+    
+    </>
+  );
+}
+
+export default App;
