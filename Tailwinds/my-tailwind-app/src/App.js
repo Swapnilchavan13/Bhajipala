ef65d370d99025b1597b4037dddd43675111a691
swapnil chavan <chavanswapnil822@gmail.com>
2024-01-08 11:13:12 +0530

    update

--- Tailwinds/my-tailwind-app/src/App.js
+++ Tailwinds/my-tailwind-app/src/App.js
@@ -4,13 +4,13 @@ import React from 'react';
 function App() {
   return (
     <>
-    <div className="bg-blue-500 text-white p-4">
-      <div className='flex gp 2'>
-        <h1 className='text-3xl'>Hello</h1>
-        <h2>Bye</h2>
-        <h1>Namaste</h1>
+    <div className="bg-black text-white p-4">
+      <div className='flex justify-between	'>
+        <h1 className='text-3xl border-2 p-10'>Hello</h1>
+        <h2 className='text-3xl border-2 p-10'>Bye</h2>
+        <h1 className='text-3xl border-2 p-10'>Namaste</h1>
       </div>
-      <h1 className="text-3xl font-bold">Hello, Tailwind CSS!</h1>
+      <h1 className="text-3xl font-bold text-red-500">Hello, Tailwind CSS!</h1>
       <p className="mt-2">This is a React app with Tailwind CSS styling.</p>
       
     </div>
